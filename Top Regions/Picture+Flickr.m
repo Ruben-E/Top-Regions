346c3949d35e59dcd240d587e686a28dbdba0e95
Ruben Ernst <rubenernst@gmail.com>
2014-05-22 17:41:46 +0200

    Create new picture with an flickr dictionary result

--- Top Regions/Picture+Flickr.m
+++ Top Regions/Picture+Flickr.m
@@ -11,13 +11,30 @@
 
 @implementation Picture (Flickr)
 
-+ (Picture *)pictureWithFlickrInfo:(NSDictionary *)photoDictionary inManagedObjectContext:(NSManagedObjectContext *)context {
++ (Picture *)pictureWithFlickrInfo:(NSDictionary *)pictureDictionary inManagedObjectContext:(NSManagedObjectContext *)context {
     Picture *picture = nil;
     
-//    NSString *flickrId =
-//    
-//    NSFetchRequest *request = [NSFetchRequest fetchRequestWithEntityName:@"Picture"];
-//    request.predicate = [NSPredicate predicateWithFormat:@"flickrId = %@", flickrId];
+    NSString *flickrId = pictureDictionary[FLICKR_PHOTO_ID];
+    
+    NSFetchRequest *request = [NSFetchRequest fetchRequestWithEntityName:@"Picture"];
+    request.predicate = [NSPredicate predicateWithFormat:@"flickrId = %@", flickrId];
+    
+    NSError *error;
+    NSArray *matches = [context executeFetchRequest:request error:&error];
+    
+    if (!matches || error || [matches count] > 1) {
+        // Handle error
+    } else if ([matches count]) {
+        picture = [matches firstObject];
+    } else {
+        picture = [NSEntityDescription insertNewObjectForEntityForName:@"Picture" inManagedObjectContext:context];
+        picture.flickrId = flickrId;
+        picture.title = [pictureDictionary valueForKeyPath:FLICKR_PHOTO_TITLE];
+        picture.subtitle = [pictureDictionary valueForKeyPath:FLICKR_PHOTO_DESCRIPTION];
+        picture.url = [[FlickrFetcher URLforPhoto:pictureDictionary format:FlickrPhotoFormatLarge] absoluteString];
+        
+        NSString *photographerName = [pictureDictionary valueForKeyPath:FLICKR_PHOTO_OWNER];
+    }
     
     return picture;
 }
