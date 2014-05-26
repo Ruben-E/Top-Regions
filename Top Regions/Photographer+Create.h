29618ec1834debbb5443fe0b116afc116c360945
Ruben Ernst <rubenernst@gmail.com>
2014-05-26 17:28:12 +0200

    Create category Create for Photographer

---
+++ Top Regions/Photographer+Create.h
@@ -0,0 +1,13 @@
+//
+//  Photographer+Create.h
+//  Top Regions
+//
+//  Created by Ruben Ernst on 26-05-14.
+//  Copyright (c) 2014 Ruben Ernst. All rights reserved.
+//
+
+#import "Photographer.h"
+
+@interface Photographer (Create)
+
+@end
---
+++ Top Regions/Photographer+Create.m
@@ -0,0 +1,13 @@
+//
+//  Photographer+Create.m
+//  Top Regions
+//
+//  Created by Ruben Ernst on 26-05-14.
+//  Copyright (c) 2014 Ruben Ernst. All rights reserved.
+//
+
+#import "Photographer+Create.h"
+
+@implementation Photographer (Create)
+
+@end
