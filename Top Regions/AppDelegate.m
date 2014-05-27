a8bf09b8c4ba1bc5e4cc59e0e083eaba9359ad2f
Ruben Ernst <rubenernst@gmail.com>
2014-05-27 18:10:51 +0200

    Implemented the DatabaseHelper with a UIManagedDocument

--- Top Regions/AppDelegate.m
+++ Top Regions/AppDelegate.m
@@ -7,13 +7,112 @@
 //
 
 #import "AppDelegate.h"
+#import "FlickrFetcher.h"
+#import "Picture+Flickr.h"
+#import "DatabaseHelper.h"
+
+@interface AppDelegate() <NSURLSessionDownloadDelegate>
+@property (copy, nonatomic) void (^flickrDownloadBackgroundURLSessionCompletionHandler)();
+@property (strong, nonatomic) NSURLSession *flickrDownloadSession;
+@property (strong, nonatomic) NSTimer *flickrForegroundFetchTimer;
+@end
+
+#define FLICKR_FETCH @"Flickr just uploaded fetch"
+#define FOREGROUND_FLICKR_FETCH_INTERVAL (20*60)
 
 @implementation AppDelegate
 
 - (BOOL)application:(UIApplication *)application didFinishLaunchingWithOptions:(NSDictionary *)launchOptions
 {
-    // Override point for customization after application launch.
+    [self startFlickrFetch];
     return YES;
+}
+
+- (void)startFlickrFetch {
+    [self.flickrDownloadSession getTasksWithCompletionHandler:^(NSArray *dataTasks, NSArray *uploadTasks, NSArray *downloadTasks) {
+        if (![downloadTasks count]) {
+            NSURLSessionDownloadTask *task = [self.flickrDownloadSession downloadTaskWithURL:[FlickrFetcher URLforRecentGeoreferencedPhotos]];
+            task.taskDescription = FLICKR_FETCH;
+            [task resume];
+        } else {
+            for (NSURLSessionDownloadTask *task in downloadTasks) {
+                [task resume];
+            }
+        }
+    }];
+}
+
+- (NSURLSession *)flickrDownloadSession
+{
+    if (!_flickrDownloadSession) {
+        static dispatch_once_t onceToken;
+        dispatch_once(&onceToken, ^{
+            NSURLSessionConfiguration *urlSessionConfig = [NSURLSessionConfiguration backgroundSessionConfiguration:FLICKR_FETCH];
+            urlSessionConfig.allowsCellularAccess = NO;
+            _flickrDownloadSession = [NSURLSession sessionWithConfiguration:urlSessionConfig delegate:self delegateQueue:nil];
+        });
+    }
+    
+    return _flickrDownloadSession;
+}
+
+- (NSArray *)flickrPhotosAtURL:(NSURL *)url
+{
+    NSDictionary *flickrPropertyList;
+    NSData *flickrJSONData = [NSData dataWithContentsOfURL:url];  // will block if url is not local!
+    if (flickrJSONData) {
+        flickrPropertyList = [NSJSONSerialization JSONObjectWithData:flickrJSONData
+                                                             options:0
+                                                               error:NULL];
+    }
+    return [flickrPropertyList valueForKeyPath:FLICKR_RESULTS_PHOTOS];
+}
+
+- (void)loadFlickrPhotosFromLocalURL:(NSURL *)localFile
+                         intoContext:(NSManagedObjectContext *)context
+                 andThenExecuteBlock:(void(^)())whenDone
+{
+    if (context) {
+        NSArray *pictures = [self flickrPhotosAtURL:localFile];
+        [context performBlock:^{
+            [Picture loadPicturesFromFlickrArray:pictures intoManagedObjectContext:context];
+            [context save:NULL]; // NOT NECESSARY if this is a UIManagedDocument's context
+            if (whenDone) whenDone();
+        }];
+    } else {
+        if (whenDone) whenDone();
+    }
+}
+
+#pragma mark - NSURLSessionDownloadDelegate
+
+- (void)URLSession:(NSURLSession *)session downloadTask:(NSURLSessionDownloadTask *)downloadTask didFinishDownloadingToURL:(NSURL *)localFile
+{
+    if ([downloadTask.taskDescription isEqualToString:FLICKR_FETCH]) {
+        [self loadFlickrPhotosFromLocalURL:localFile
+                               intoContext:[DatabaseHelper context]
+                       andThenExecuteBlock:^{
+                           [self flickrDownloadTasksMightBeComplete];
+                       }
+         ];
+    }
+}
+
+// required by the protocol
+- (void)URLSession:(NSURLSession *)session
+      downloadTask:(NSURLSessionDownloadTask *)downloadTask
+ didResumeAtOffset:(int64_t)fileOffset
+expectedTotalBytes:(int64_t)expectedTotalBytes
+{
+}
+
+// required by the protocol
+- (void)URLSession:(NSURLSession *)session
+      downloadTask:(NSURLSessionDownloadTask *)downloadTask
+      didWriteData:(int64_t)bytesWritten
+ totalBytesWritten:(int64_t)totalBytesWritten
+totalBytesExpectedToWrite:(int64_t)totalBytesExpectedToWrite
+{
 }
 
 
--- Top Regions/DatabaseHelper.h
+++ Top Regions/DatabaseHelper.h
@@ -5,11 +5,19 @@
 //  Created by Ruben Ernst on 26-05-14.
 //  Copyright (c) 2014 Ruben Ernst. All rights reserved.
 //
+//  Inspiration: http://stackoverflow.com/questions/14876988/core-data-uimanageddocument-or-appdelegate-to-setup-core-data-stack
+//
 
 #import <Foundation/Foundation.h>
 
 @interface DatabaseHelper : NSObject
 
-+ (UIManagedDocument *)managedDocument;
+//+ (UIManagedDocument *)managedDocument;
+
+
+- (void)saveContext;
++ (id) sharedInstance;
++ (void)disposeInstance;
++ (NSManagedObjectContext *)context;
 
 @end
