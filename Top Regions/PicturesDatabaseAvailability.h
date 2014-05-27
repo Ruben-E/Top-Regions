0cd5761b99279d8fa31d792574f8804cf16c34fc
Ruben Ernst <rubenernst@gmail.com>
2014-05-27 18:26:42 +0200

    Created header file for the broadcast (notification)

---
+++ Top Regions/PicturesDatabaseAvailability.h
@@ -0,0 +1,15 @@
+//
+//  PicturesDatabaseAvailability.h
+//  Top Regions
+//
+//  Created by Ruben Ernst on 27-05-14.
+//  Copyright (c) 2014 Ruben Ernst. All rights reserved.
+//
+
+#ifndef Top_Regions_PicturesDatabaseAvailability_h
+#define Top_Regions_PicturesDatabaseAvailability_h
+
+#define PicturesDatabaseAvailabilityNotification @"PicturesDatabaseAvailabilityNotification"
+#define PicturesDatabaseAvailabilityContext @"Context"
+
+#endif
