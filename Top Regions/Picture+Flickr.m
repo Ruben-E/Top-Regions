ef83afb5e8710e4366bd476515b4645618021133
Ruben Ernst <rubenernst@gmail.com>
2014-05-31 13:01:11 +0200

    Saving the pictures from flickr async

--- Top Regions/Picture+Flickr.m
+++ Top Regions/Picture+Flickr.m
@@ -16,76 +16,74 @@
 
 @implementation Picture (Flickr)
 
-+ (Picture *)pictureWithFlickrInfo:(NSDictionary *)pictureDictionary inManagedObjectContext:(NSManagedObjectContext *)context {
-    Picture *picture = nil;
-    
-    NSString *flickrId = pictureDictionary[FLICKR_PHOTO_ID];
-    
-    NSFetchRequest *request = [NSFetchRequest fetchRequestWithEntityName:@"Picture"];
-    request.predicate = [NSPredicate predicateWithFormat:@"flickrId = %@", flickrId];
-    
-    NSError *error;
-    NSArray *matches = [context executeFetchRequest:request error:&error];
-    
-    if (!matches || error || [matches count] > 1) {
-        // Handle error
-    } else if ([matches count]) {
-        picture = [matches firstObject];
-    } else {
-        NSURL *urlPlace =[FlickrFetcher URLforInformationAboutPlace:[pictureDictionary valueForKeyPath: FLICKR_PHOTO_PLACE_ID]];
-        NSData *jsonResults = [NSData dataWithContentsOfURL:urlPlace];
-        NSDictionary *placeInformation =[NSJSONSerialization JSONObjectWithData:jsonResults
-                                                                        options:0
-                                                                          error:NULL];
++ (void)pictureWithFlickrInfo:(NSDictionary *)pictureDictionary inManagedObjectContext:(NSManagedObjectContext *)context {
 
-        picture = [NSEntityDescription insertNewObjectForEntityForName:@"Picture" inManagedObjectContext:context];
-        picture.flickrId = flickrId;
-        picture.title = [pictureDictionary valueForKeyPath:FLICKR_PHOTO_TITLE];
-        picture.subtitle = [pictureDictionary valueForKeyPath:FLICKR_PHOTO_DESCRIPTION];
-        picture.url = [[FlickrFetcher URLforPhoto:pictureDictionary format:FlickrPhotoFormatLarge] absoluteString];
-        
-        NSString *photographerName = [pictureDictionary valueForKeyPath:FLICKR_PHOTO_OWNER];
-        NSString *photographerId = [pictureDictionary valueForKeyPath:FLICKR_PHOTO_OWNER_ID];
-
-        NSString *placeName = [FlickrFetcher extractNameOfPlace:[pictureDictionary valueForKeyPath:FLICKR_PHOTO_PLACE_ID] fromPlaceInformation:pictureDictionary];
-        NSString *placeId = [pictureDictionary valueForKeyPath:FLICKR_PHOTO_PLACE_ID];
-
-        NSString *regionName = [FlickrFetcher extractRegionNameFromPlaceInformation:placeInformation];
-        NSString *regionId = [FlickrFetcher extractRegionIdFromPlaceInformation:placeInformation];
-
-        NSEntityDescription *photographerEntity = [NSEntityDescription entityForName:@"Photographer" inManagedObjectContext:context];
-        NSEntityDescription *placeEntity = [NSEntityDescription entityForName:@"Place" inManagedObjectContext:context];
-        NSEntityDescription *regionEntity = [NSEntityDescription entityForName:@"Region" inManagedObjectContext:context];
-
-        Photographer *photographerDomain = [[NSManagedObject alloc] initWithEntity:photographerEntity insertIntoManagedObjectContext:nil];
-        photographerDomain.flickrId = photographerId;
-        photographerDomain.name = photographerName;
-
-        Place *placeDomain = [[NSManagedObject alloc] initWithEntity:placeEntity insertIntoManagedObjectContext:nil];
-        placeDomain.flickrId = placeId;
-        placeDomain.name = placeName;
-
-        Region *regionDomain = [[NSManagedObject alloc] initWithEntity:regionEntity insertIntoManagedObjectContext:nil];
-        regionDomain.flickrId = regionId;
-        regionDomain.name = regionName;
-
-        Place *place = [Place PlaceByPlace:placeDomain inManagedObjectContext:context];
-        Region *region = [Region RegionByRegion:regionDomain inManagedObjectContext:context];
-        Photographer *photographer = [Photographer photographerByPhotographer:photographerDomain inManagedObjectContext:context];
-
-        place.isIn = region;
-
-        picture.takenIn = place;
-        picture.whoTook = photographer;
-    }
-    
-    return picture;
 }
 
 + (void)loadPicturesFromFlickrArray:(NSArray *)pictures intoManagedObjectContext:(NSManagedObjectContext *)context {
-    for (NSDictionary *picture in pictures) {
-        [self pictureWithFlickrInfo:picture inManagedObjectContext:context];
-    }
+    dispatch_queue_t placeQ =dispatch_queue_create("place fetcher", NULL);
+    dispatch_async(placeQ, ^{
+        for (NSDictionary *pictureDicionary in pictures) {
+            //[self pictureWithFlickrInfo:picture inManagedObjectContext:context];
+            NSURL *urlPlace = [FlickrFetcher URLforInformationAboutPlace:[pictureDicionary valueForKeyPath:FLICKR_PHOTO_PLACE_ID]];
+            NSData *jsonResults = [NSData dataWithContentsOfURL:urlPlace];
+            NSDictionary *placeInformation = [NSJSONSerialization JSONObjectWithData:jsonResults
+                                                                             options:0
+                                                                               error:NULL];
+            dispatch_async(dispatch_get_main_queue(), ^{
+                NSString *flickrId = pictureDicionary[FLICKR_PHOTO_ID];
+
+                NSFetchRequest *request = [NSFetchRequest fetchRequestWithEntityName:@"Picture"];
+                request.predicate = [NSPredicate predicateWithFormat:@"flickrId = %@", flickrId];
+
+                NSError *error;
+                NSArray *matches = [context executeFetchRequest:request error:&error];
+
+                if (matches && !error && [matches count] == 0) {
+                    Picture *picture = [NSEntityDescription insertNewObjectForEntityForName:@"Picture" inManagedObjectContext:context];
+                    picture.flickrId = flickrId;
+                    picture.title = [pictureDicionary valueForKeyPath:FLICKR_PHOTO_TITLE];
+                    picture.subtitle = [pictureDicionary valueForKeyPath:FLICKR_PHOTO_DESCRIPTION];
+//                    picture.subtitle = @"";
+                    picture.url = [[FlickrFetcher URLforPhoto:pictureDicionary format:FlickrPhotoFormatLarge] absoluteString];
+
+                    NSString *photographerName = [pictureDicionary valueForKeyPath:FLICKR_PHOTO_OWNER];
+                    NSString *photographerId = [pictureDicionary valueForKeyPath:FLICKR_PHOTO_OWNER_ID];
+
+                    NSString *placeName = [FlickrFetcher extractNameOfPlace:[pictureDicionary valueForKeyPath:FLICKR_PHOTO_PLACE_ID] fromPlaceInformation:pictureDicionary];
+                    NSString *placeId = [pictureDicionary valueForKeyPath:FLICKR_PHOTO_PLACE_ID];
+
+                    NSString *regionName = [FlickrFetcher extractRegionNameFromPlaceInformation:placeInformation];
+                    NSString *regionId = [FlickrFetcher extractRegionIdFromPlaceInformation:placeInformation];
+
+                    NSEntityDescription *photographerEntity = [NSEntityDescription entityForName:@"Photographer" inManagedObjectContext:context];
+                    NSEntityDescription *placeEntity = [NSEntityDescription entityForName:@"Place" inManagedObjectContext:context];
+                    NSEntityDescription *regionEntity = [NSEntityDescription entityForName:@"Region" inManagedObjectContext:context];
+
+                    Photographer *photographerDomain = [[NSManagedObject alloc] initWithEntity:photographerEntity insertIntoManagedObjectContext:nil];
+                    photographerDomain.flickrId = photographerId;
+                    photographerDomain.name = photographerName;
+
+                    Place *placeDomain = [[NSManagedObject alloc] initWithEntity:placeEntity insertIntoManagedObjectContext:nil];
+                    placeDomain.flickrId = placeId;
+                    placeDomain.name = placeName;
+
+                    Region *regionDomain = [[NSManagedObject alloc] initWithEntity:regionEntity insertIntoManagedObjectContext:nil];
+                    regionDomain.flickrId = regionId;
+                    regionDomain.name = regionName;
+
+                    Place *place = [Place PlaceByPlace:placeDomain inManagedObjectContext:context];
+                    Region *region = [Region RegionByRegion:regionDomain inManagedObjectContext:context];
+                    Photographer *photographer = [Photographer photographerByPhotographer:photographerDomain inManagedObjectContext:context];
+
+                    place.isIn = region;
+
+                    picture.takenIn = place;
+                    picture.whoTook = photographer;
+                }
+            });
+        }
+    });
 }
 
 @end
