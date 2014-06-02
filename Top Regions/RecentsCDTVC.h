68df4b43530cc9fdafc2afb594c683e994512622
Ruben Ernst <rubenernst@gmail.com>
2014-06-02 17:26:18 +0200

    [REQ 5] Created the CDTVC for recent pictures. Displays max 50 pictures, sorted by date

---
+++ Top Regions/RecentsCDTVC.h
@@ -0,0 +1,14 @@
+//
+// Created by Ruben Ernst on 02-06-14.
+// Copyright (c) 2014 Ruben Ernst. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+#import "CoreDataTableViewController.h"
+
+
+@interface RecentsCDTVC : CoreDataTableViewController
+
+@property (nonatomic, strong) NSManagedObjectContext *managedObjectContext;
+
+@end
---
+++ Top Regions/RecentsCDTVC.m
@@ -0,0 +1,63 @@
+//
+// Created by Ruben Ernst on 02-06-14.
+// Copyright (c) 2014 Ruben Ernst. All rights reserved.
+//
+
+#import "RecentsCDTVC.h"
+#import "PicturesDatabaseAvailability.h"
+#import "Picture.h"
+#import "PictureViewController.h"
+
+
+@implementation RecentsCDTVC
+
+- (void)awakeFromNib {
+    [[NSNotificationCenter defaultCenter] addObserverForName:PicturesDatabaseAvailabilityNotification object:nil queue:nil usingBlock:^(NSNotification *notification){
+        self.managedObjectContext = notification.userInfo[PicturesDatabaseAvailabilityContext];
+    }];
+}
+
+- (void)setManagedObjectContext:(NSManagedObjectContext *)managedObjectContext {
+    _managedObjectContext = managedObjectContext;
+
+    NSFetchRequest *request = [NSFetchRequest fetchRequestWithEntityName:@"Picture"];
+    request.predicate = nil;
+    request.sortDescriptors = @[[NSSortDescriptor sortDescriptorWithKey:@"uploadedAt" ascending:NO]];
+    request.fetchLimit = 50;
+
+    self.fetchedResultsController = [[NSFetchedResultsController alloc] initWithFetchRequest:request managedObjectContext:managedObjectContext sectionNameKeyPath:nil cacheName:nil];
+}
+
+- (UITableViewCell *)tableView:(UITableView *)tableView cellForRowAtIndexPath:(NSIndexPath *)indexPath {
+    UITableViewCell *cell = [self.tableView dequeueReusableCellWithIdentifier:@"Cell"];
+
+    Picture *picture = [self.fetchedResultsController objectAtIndexPath:indexPath];
+
+    cell.textLabel.text = picture.title;
+    cell.detailTextLabel.text = picture.description;
+
+    return cell;
+}
+
+- (void)prepareImagePictureViewController:(PictureViewController *)pictureViewController toDisplayPicture:(Picture *)picture {
+    pictureViewController.pictureURL = [NSURL URLWithString:picture.url];
+    pictureViewController.pictureTitle = picture.title;
+    pictureViewController.title = picture.title;
+}
+
+- (void)prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender {
+    if ([segue.identifier isEqualToString:@"ShowPicture"]) {
+        PictureViewController *pictureViewController = (PictureViewController *)[segue destinationViewController];
+
+        UITableViewCell *cell = (UITableViewCell*)sender;
+        NSIndexPath *indexPath = [self.tableView indexPathForCell:cell];
+
+        Picture *picture = [self.fetchedResultsController objectAtIndexPath:indexPath];
+
+        if (picture) {
+            [self prepareImagePictureViewController:pictureViewController toDisplayPicture:picture];
+        }
+    }
+}
+
+@end
